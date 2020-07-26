79a119afd8d5ea851ca2e2aef307e6923df5a856
TheShubham-K <subham.kumar03@gmail.com>
2020-07-26 09:04:46 +0530

    Updated

--- js/script.js
+++ js/script.js
@@ -26,3 +26,39 @@ $(document).ready(function() {
         $('#LoginModal').modal('toggle');
     });
 });
+
+// $(window).scroll(function() {
+//     $(".top").css("opacity", 1 - $(window).scrollTop() / 250);
+// });
+
+// $(document).scroll(function() {
+//     var scrollVar = $(document).scrollTop();
+//     $('.top').css("opacity", (scrollVar / 300) - 1);
+// })
+
+$(document).on("wheel", function() {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $("section")
+    var tags1 = $("div")
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i]
+
+        if ($(tag).position().top > pageBottom) {
+            $(tag).removeClass("visible")
+        } else {
+            $(tag).addClass("visible")
+        }
+    }
+
+    for (var i = 0; i < tags1.length; i++) {
+        var tag = tags1[i]
+
+        if ($(tag).position().top > pageBottom) {
+            $(tag).removeClass("visible")
+        } else {
+            $(tag).addClass("visible")
+        }
+    }
+})
